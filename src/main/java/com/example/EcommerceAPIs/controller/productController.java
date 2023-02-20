bea7d93b209b0b34f467fafa20fb36d90225e84c
kalpesh patil <kalpesh123.kp78@gmail.com>
2023-02-20 12:43:47 +0530

    Created get product  and delete produact API
    Created get product(Get product by category with the use of request param) also created delete product api

--- src/main/java/com/example/EcommerceAPIs/controller/productController.java
+++ src/main/java/com/example/EcommerceAPIs/controller/productController.java
@@ -2,11 +2,11 @@ package com.example.EcommerceAPIs.controller;
 
 import com.example.EcommerceAPIs.model.Product;
 import com.example.EcommerceAPIs.service.productService;
+import jakarta.annotation.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class productController {
@@ -16,5 +16,16 @@ public class productController {
     @PostMapping("add-product")
     public Product addProduct(@RequestBody Product product){
         return productservice.addProduct(product);
+    }
+    
+    @GetMapping("get-products")
+    public List<Product> getProducts(@Nullable @RequestParam String category){
+        return productservice.getProducts(category);
+    }
+
+    @DeleteMapping("delete-product")
+    public String deleteProduct(@RequestParam Integer id){
+        productservice.deleteProduct(id);
+        return "deleted successfully";
     }
 }
--- src/main/java/com/example/EcommerceAPIs/service/productService.java
+++ src/main/java/com/example/EcommerceAPIs/service/productService.java
@@ -5,6 +5,9 @@ import com.example.EcommerceAPIs.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class productService {
     @Autowired
@@ -12,5 +15,25 @@ public class productService {
 
     public Product addProduct(Product product){
         return productrepository.save(product);
+    }
+
+
+    public List<Product> getProducts(String category){
+        List<Product>products=productrepository.findAll();
+        List<Product>filterdproducts=new ArrayList<>();
+        if(category!=null){
+            for(Product product:products){
+                if(product.getCategory().equals(category)){
+                    filterdproducts.add(product);
+                }
+            }
+            return filterdproducts;
+        }
+
+
+        return products;
+    }
+    public void deleteProduct(Integer id){
+         productrepository.deleteById(id);
     }
 }
